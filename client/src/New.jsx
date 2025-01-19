d8023cb248bbd1f3cbcdb8df01e9d22894603c73
konhito <konhito02@gmail.com>
2025-01-19 20:10:44 +0530

    Initiated a worker for data extraction

--- client/src/New.jsx
+++ client/src/New.jsx
@@ -42,7 +42,7 @@ function Addques() {
       />
       <button
         onClick={handleSubmit}
-        className="bg-blue-600 text-white rounded-r-md px-4 py-2 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="bg-blue-600  text-white rounded-r-md px-4 py-2 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
       >
         Submit
       </button>
